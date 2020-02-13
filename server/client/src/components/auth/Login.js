6dd0127e445345f28cdc44b03d21406936d707c3
Shakti Gurung <mr.shakti.gurung@gmail.com>
2020-02-13 13:10:02 +1100

    redux implemented throughout the application

--- server/client/src/components/auth/Login.js
+++ server/client/src/components/auth/Login.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../redux/actions/authActions";
+import classnames from "classnames";
+
 class Login extends Component {
     constructor() {
     super();
@@ -8,6 +13,25 @@ class Login extends Component {
         password: "",
         errors: {}
         };
+    }
+
+    componentDidMount() {
+        // If logged in and user navigates to Login page, should redirect them to dashboard
+        if (this.props.auth.isAuthenticated) {
+          this.props.history.push("/dashboard");
+        }
+    }
+    
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.auth.isAuthenticated) {
+          this.props.history.push("/dashboard"); // push user to dashboard when they login
+        }
+
+        if (nextProps.errors) {
+          this.setState({
+            errors: nextProps.errors
+          });
+        }
     }
 
     onChange = e => {
@@ -21,6 +45,11 @@ class Login extends Component {
         password: this.state.password
         };
         console.log(userData);
+        this.props.loginUser(userData);
+        this.setState({
+            email: "",
+            password: ""
+        })
     };
     render() {
         const { errors } = this.state;
@@ -43,23 +72,37 @@ class Login extends Component {
                     <form noValidate onSubmit={this.onSubmit}>
                     <div className="input-field col s12">
                         <input
-                        onChange={this.onChange}
-                        value={this.state.email}
-                        error={errors.email}
-                        id="email"
-                        type="email"
+                            onChange={this.onChange}
+                            value={this.state.email}
+                            error={errors.email}
+                            id="email"
+                            type="email"
+                            className={classnames("", {
+                                invalid: errors.email || errors.emailnotfound
+                            })}
                         />
                         <label htmlFor="email">Email</label>
+                        <span className="red-text">
+                            {errors.email}
+                            {errors.emailnotfound}
+                        </span>
                     </div>
                     <div className="input-field col s12">
                         <input
-                        onChange={this.onChange}
-                        value={this.state.password}
-                        error={errors.password}
-                        id="password"
-                        type="password"
+                            onChange={this.onChange}
+                            value={this.state.password}
+                            error={errors.password}
+                            id="password"
+                            type="password"
+                            className={classnames("", {
+                                invalid: errors.password || errors.passwordincorrect
+                            })}
                         />
                         <label htmlFor="password">Password</label>
+                        <span className="red-text">
+                            {errors.password}
+                            {errors.passwordincorrect}
+                        </span>
                     </div>
                     <div className="col s12" style={{ paddingLeft: "11.250px" }}>
                         <button
@@ -82,4 +125,19 @@ class Login extends Component {
         );
   }
 }
-export default Login;
+
+Login.propTypes = {
+    loginUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth,
+    errors: state.errors
+});
+
+export default connect(
+    mapStateToProps,
+    { loginUser }
+)(Login);
