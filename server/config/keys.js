bfdbcf4b864900eb75b8619d2992d0ff3b745b5a
Shakti Gurung <mr.shakti.gurung@gmail.com>
2020-02-13 11:48:02 +1100

    server and client setup

---
+++ server/config/keys.js
@@ -0,0 +1,7 @@
+
+const YOUR_MONGOURI_HERE = "mongodb://youtube-admin:admin123#@ds337718.mlab.com:37718/youtube";
+
+module.exports = {
+    mongoURI: YOUR_MONGOURI_HERE,
+    secretOrKey: "secret"
+  };
